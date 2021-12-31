8357f7e8b79517619d2f51de77fed31288157a93
jon <jonathanyusf@gmail.com>
2021-12-30 21:31:37 -0800

    Fix image positioning plus, fix classname syntax bug

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -5,7 +5,7 @@ import ai from "../../assets/ai.png";
 
 const Header = () => {
   return (
-    <div classname="gpt3__header section__padding" id="home">
+    <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
         <h1 className="gradient__text">
           Let’s Build Something amazing with GPT-3 OpenAI
@@ -24,9 +24,9 @@ const Header = () => {
           <img src={people} alt="people" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-        <div className="gpt3__header-image">
-          <img src={ai} alt="ai" />
-        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
       </div>
     </div>
   );
