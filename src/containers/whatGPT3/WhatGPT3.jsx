0806e75c05db55a8efec46005fd5a59db9dec2fa
jon <jonathanyusf@gmail.com>
2022-01-04 23:46:07 -0800

    Start on heading

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -8,8 +8,13 @@ const WhatGPT3 = () => {
       <div className="gpt3__whatgpt3-feature">
         <Feature />
       </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text">
+          The possibilities are beyond your imagination
+        </h1>
+      </div>
     </div>
-  )
+  );
 };
 
 export default WhatGPT3;
