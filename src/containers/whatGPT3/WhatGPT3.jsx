c904fec1eb2b966d2e3d5c5e6643c610a03e4168
jon <jonathanyusf@gmail.com>
2022-01-04 21:04:04 -0800

    Start on WhatGPT3 component

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { Feature } from "../../components";
 import "./whatgpt3.css";
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section_margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+    </div>
+  )
 };
 
 export default WhatGPT3;
